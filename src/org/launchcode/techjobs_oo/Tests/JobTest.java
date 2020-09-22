ef24b0f620bf9c97c23175b2fda858873e590dcb
Milan Shrestha <milanshrestha17@hotmail.com>
2020-09-21 21:13:49 -0400

    Added all Job insantiations to @BeforeClass to discard tests interferences

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,6 +1,6 @@
 package org.launchcode.techjobs_oo.Tests;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
 
@@ -8,18 +8,42 @@ import static org.junit.Assert.*;
 
 public class JobTest {
 
-    Job one;
-    Job two;
+    private static Job one;
+    private static Job two;
+    private static Job three;
+    private static Job four;
+    private static Job five;
+    private static Job six;
+    private static Job seven;
+    private static Job eight;
 
-    @Before
-    public void createJobsWithConstructor() {
+    @BeforeClass
+    public static void createJobsWithConstructor() {
 
         one = new Job();
+
         two = new Job();
+
+        three = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        four = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
+                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
+
+        five = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
+                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
+
+        six = new Job();
+
+        seven = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
+                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
+
+        eight = new Job("", new Employer("Google"), new Location("Silicon Valley"),
+                new PositionType("UX Designer"), new CoreCompetency(""));
     }
 
-    //Testing empty constructors
 
+    // Testing empty constructors
     @Test
     public void testSettingJobId() {
 
@@ -28,13 +52,10 @@ public class JobTest {
         assertFalse("false", two.getId() != 2);
     }
 
-    //Testing full constructors with checks for proper assignment of classes and values related to them
 
+    // Testing full constructors with checks for proper assignment of classes and values related to them
     @Test
     public void testJobConstructorSetsAllFields() {
-
-        Job three = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
-                new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         assertEquals(3,three.getId());
         assertEquals("Product tester", three.getName());
@@ -48,39 +69,30 @@ public class JobTest {
         assertTrue("true", three.getCoreCompetency() instanceof CoreCompetency);
     }
 
-    //Testing custom equals method
 
+    // Testing custom equals method
     @Test
     public void testJobsForEquality() {
-
-        Job four = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
-                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
-
-        Job five = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
-                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
 
         assertFalse("false", four.equals(five));
     }
 
+
+    // Testing custom toString method
     @Test
     public void testToString() {
 
-        //Testing for blank lines before and after the job information
-        //Testing for “OOPS! This job does not seem to exist.” message when only "id" is available
-
-        Job six = new Job();
+        // Testing for blank lines before and after the job information
+        // Testing for “OOPS! This job does not seem to exist.” message when only "id" is available
 
         assertEquals("\n" +
                 "OOPS! This job does not seem to exist." +
                 "\n", six.toString());
 
-        //Testing for proper labels and assigned values in the job information
-
-        Job seven = new Job("John Smith", new Employer("Google"), new Location("Silicon Valley"),
-                new PositionType("UX Designer"), new CoreCompetency("Problem Solving"));
+        // Testing for proper labels and assigned values in the job information
 
         assertEquals("\n" +
-                "ID: " + 4 + "\n" +
+                "ID: " + 7 + "\n" +
                 "Name: " + "John Smith" + "\n" +
                 "Employer: " + "Google" + "\n" +
                 "Location: " + "Silicon Valley" + "\n" +
@@ -88,13 +100,10 @@ public class JobTest {
                 "Core Competency: " + "Problem Solving" +
                 "\n", seven.toString());
 
-        //Testing for correct message "Data not available"  displayed when field/s left empty
-
-        Job eight = new Job("", new Employer("Google"), new Location("Silicon Valley"),
-                new PositionType("UX Designer"), new CoreCompetency(""));
+        // Testing for correct message "Data not available"  displayed when field/s left empty
 
         assertEquals("\n" +
-                "ID: " + 5 + "\n" +
+                "ID: " + 8 + "\n" +
                 "Name: " + "Data not available" + "\n" +
                 "Employer: " + "Google" + "\n" +
                 "Location: " + "Silicon Valley" + "\n" +
